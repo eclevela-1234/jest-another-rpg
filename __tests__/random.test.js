7282fa2bebc4dd5fcb6c8150d8e11da6e2e33498
Eliot Cleveland <eclevela@richmond.edu>
2022-07-27 23:40:39 -0400

    code snapshot

--- __tests__/random.test.js
+++ __tests__/random.test.js
@@ -1,6 +1,6 @@
-const randomNumber = require('../lib/random.js');
+const randomNumber = require('../lib/random');
 
-test('gets random number between 1 and 10', () => {
-    expect(randomNumber()).toBeGreaterThanOrEqual(1);
-    expect(randomNumber()).toBeLessThanOrEqual(10);
+test('generates a random number between 1 and 10', () => {
+  expect(randomNumber()).toBeGreaterThanOrEqual(1);
+  expect(randomNumber()).toBeLessThanOrEqual(10);
 });
